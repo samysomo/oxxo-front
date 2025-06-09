be5df062c7bb09a6c0b55d48308ec5f9f9848992
Samuel Serrato <samysd12@hotmail.com>
2025-06-08 18:48:51 -0600

    fix employee/id/page

--- app/dashboard/employees/[id]/page.tsx
+++ app/dashboard/employees/[id]/page.tsx
@@ -1,10 +1,8 @@
 import authHeaders from '@/helpers/authHeaders'
 import React from 'react'
 import EmployeeCard from '../_components/EmployeeCard'
-import { getUserRoles } from '@/helpers/getUserRoles'
 
 const API_URL = process.env.API_URL
-const role = getUserRoles()
 
 const EmployeePage = async ({params} : {params: {id : string}}) => {
   const response = await fetch(`${API_URL}/employees/${params.id}`, {
@@ -16,7 +14,7 @@ const EmployeePage = async ({params} : {params: {id : string}}) => {
   const employeeData : Employee = await response.json()
   return (
     <div className='w-10/12 h-[90vh] flex items-center justify-center'>
-      <EmployeeCard userRole={role[0]} employee={employeeData} main={true} hover={false} full={true}/>
+      <EmployeeCard userRole="employee" employee={employeeData} main={true} hover={false} full={true}/>
     </div>
   )
 }
